cc6c436fb9fcaac21bddb8e67f3c0fd2ab445ab3
L V Ramana <lellalvramana@gmail.com>
2026-01-29 06:26:04 +0530

    updated terraform

--- Practice/DockerInstance/Data.tf
+++ Practice/DockerInstance/Data.tf
@@ -1,10 +1,19 @@
-data"aws_ami""joindevops"{
-    owners=["973714476881"]
+data "aws_ami" "joindevops"{
     most_recent = true
 
-    filter{
-        name="name"
-        values=["RHEL-9-DevOps-Practice"]
-    }
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }     
+
+  owners = ["973714476881"] 
 }
